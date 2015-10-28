693b74c534d8face96d639899911c27d1dcb9988
Christian Aranda <christian.aranda@gmail.com>
2015-10-28 17:29:35 +0100

    ENH: Add AV foundation Capture Session in order to gain control over Video recordings.

---
+++ CaptureSessionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaptureSessionViewController.h
+//  SimpleVideoRecorder
+//
+//  Created by Alma IT on 28/10/15.
+//  Copyright (c) 2015 Alma IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptureSessionViewController : UIViewController
+
+@end
